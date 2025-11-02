e9086032c21a34ee89aa99f1a24a74d33df51961
Rajae Gregory <rajgreg306@gmail.com>
2025-11-02 17:30:58 -0500

    Complete Functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".newsletter form");
+    const input = document.querySelector("#email");
+    const message = document.querySelector(".newsletter .message");
+
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+    const email = input.value.trim();
+    
+    if (email === ""){
+        message.textContent = "Please enter a valid email address";
+
+    } else{
+        message.textContent = `Thank you! Your email address ${email} has been added to our mailing list`;
+        form.reset();
+    }
+
+    })
+
+
+})
+
+
